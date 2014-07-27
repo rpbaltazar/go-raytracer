b47ca28fbf3ad17ec68cc34fb670ce677acff2fa
Rui Baltazar <rui.p.baltazar@gmail.com>
2014-07-27 15:52:09 +0800

    initial commit
     - basic type structures
     - no libs yet
     - readme file

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Go Raytracer
+
+This project appears as a small idea of testing go language, re-using an idea that I implemented a couple of years ago.
+
+A raytracer is one of the famous CG algorithms widely implemented in different languages and using different theories behind it.
+All the theory associated with the decisions will be found in the wiki of the project.
+
+Fork the repo, and feel free to use it, modify it, make it better, faster, more beautiful, ...
+
+## System setup
+
+1. Homebrew
+2. Go
+
+## Compilation
+
+## Running
---
+++ raytrace.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+type Point3D struct {
+  X, Y, Z int
+}
+
+type Ray struct {
+  Origin, Direction Point3D
+}
+
+type Camera struct {
+  Position Point3D
+}
+
+type Sphere struct {
+  Center Point3D
+  SphereRay int
+}
+
+var imageWidth, imageHeight int = 800, 640
+
+func main() {
+
+  camera := Camera{ Point3D{X: imageWidth/2, Y: imageHeight/2, Z: -100 } }
+
+  scene := loadScene()
+
+  fmt.Println(camera)
+
+  for px := 0; px < imageWidth; px++{
+    for py :=0; py < imageHeight; py++{
+      //Create ray with origin ox, oy and direction from px, py to ox, oy
+      primRay := computeRay(px, py, camera)
+      // fmt.Println(primRay)
+      //Scene.isIntersectedBy(primRay)
+      for idx := 0; idx < len(scene); idx ++{
+        if intersects(primRay, scene[idx]){
+          fmt.Println(primRay)
+        }
+      }
+    }
+  }
+}
+
+func intersects(primRay Ray, obj Sphere) bool{
+  return true
+}
+
+func loadScene() []Sphere{
+  obj := Sphere{ Center: Point3D {X: 150, Y: 150, Z: 150}, SphereRay: 50 }
+  return []Sphere{obj}
+}
+
+func computeRay(px, py int, cam Camera) Ray {
+  rayDirX := px - cam.Position.X
+  rayDirY := py - cam.Position.Y
+  rayDirZ := -cam.Position.Z
+
+  return Ray{ Origin: cam.Position, Direction: Point3D {rayDirX, rayDirY, rayDirZ} }
+}
