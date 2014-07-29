bf4b43521a0e04ef57a1fe67adfe605cf82881d8
Rui Baltazar <rui.p.baltazar@gmail.com>
2014-07-30 00:58:37 +0800

    added functions to Point struct. started implementing intersection theory

--- raytrace.go
+++ raytrace.go
@@ -1,13 +1,31 @@
 package main
 
 import "fmt"
+import "math"
 
 type Point3D struct {
-  X, Y, Z int
+  X, Y, Z float64
 
 }
+
 func (p *Point3D) Subtract(p1 Point3D) Point3D {
   return Point3D{ X: p.X-p1.X, Y: p.Y-p1.Y, Z: p.Z-p1.Z}
+}
+
+func (p *Point3D) Add(p1 Point3D) Point3D {
+  return Point3D{ X: p.X+p1.X, Y: p.Y+p1.Y, Z: p.Z+p1.Z}
+}
+
+func (p *Point3D) Multiply(f1 float64) Point3D {
+  return Point3D{ X: p.X*f1, Y: p.Y*f1, Z: p.Z*f1}
+}
+
+func (p *Point3D) PointMultiply(p1 Point3D) Point3D {
+  return Point3D{ X: p.X*p1.X, Y: p.Y*p1.Y, Z: p.Z*p1.Z}
+}
+
+func (p *Point3D) Pow(f float64) Point3D {
+  return Point3D{ X: math.Pow(p.X, f), Y: math.Pow(p.Y, f), Z: math.Pow(p.Z, f) }
 }
 
 type Ray struct {
@@ -20,14 +38,14 @@ type Camera struct {
 
 type Sphere struct {
   Center Point3D
-  SphereRay int
+  SphereRay float64
 }
 
 var imageWidth, imageHeight int = 800, 640
 
 func main() {
 
-  camera := Camera{ Point3D{X: imageWidth/2, Y: imageHeight/2, Z: -100 } }
+  camera := Camera{ Point3D{X: float64(imageWidth)/2, Y: float64(imageHeight)/2, Z: -100 } }
 
   scene := loadScene()
 
@@ -49,7 +67,27 @@ func main() {
 }
 
 func intersects(primRay Ray, obj Sphere) bool{
-  return true
+  v := primRay.Direction
+  o := primRay.Origin
+  c := obj.Center
+  r := obj.SphereRay
+
+  t1 := v.PointMultiply(o.Subtract(c))
+  t1 = t1.Pow(2)
+
+  ti1 := o.PointMultiply(c)
+  ti1 = ti1.Multiply(2)
+
+  ti2 := o.Pow(2)
+  ti3 := c.Pow(2)
+
+  t2 := ti2.Subtract(ti1)
+  t2 = t2.Add(ti3)
+
+  t3 := math.Pow(r, 2)
+
+  fmt.Println(t3, o,c, o.Subtract(c))
+  return false
 }
 
 func loadScene() []Sphere{
@@ -58,8 +96,8 @@ func loadScene() []Sphere{
 }
 
 func computeRay(px, py int, cam Camera) Ray {
-  rayDirX := px - cam.Position.X
-  rayDirY := py - cam.Position.Y
+  rayDirX := float64(px) - cam.Position.X
+  rayDirY := float64(py) - cam.Position.Y
   rayDirZ := -cam.Position.Z
 
   return Ray{ Origin: cam.Position, Direction: Point3D {rayDirX, rayDirY, rayDirZ} }
