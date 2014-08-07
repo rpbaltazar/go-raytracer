6c5dc31f99f9b4d1def53c9b0018246244b5b8d2
Rui Baltazar <rui.p.baltazar@gmail.com>
2014-08-07 14:31:51 +0800

    test coverage for point methods improved

--- lib/scene-objects/point3d.go
+++ lib/scene-objects/point3d.go
@@ -18,10 +18,6 @@ func (p *Point3D) Multiply(f1 float64) Point3D {
   return Point3D{ X: p.X*f1, Y: p.Y*f1, Z: p.Z*f1}
 }
 
-func (p *Point3D) PointMultiply(p1 Point3D) Point3D {
-  return Point3D{ X: p.X*p1.X, Y: p.Y*p1.Y, Z: p.Z*p1.Z}
-}
-
 func (p *Point3D) ScalarProd(p1 Point3D) float64 {
   return p.X*p1.X + p.Y*p1.Y + p.Z*p1.Z
 }
--- lib/scene-objects/point3d_test.go
+++ lib/scene-objects/point3d_test.go
@@ -2,11 +2,7 @@ package scene_objects
 
 import "testing"
 
-func TestSubtract(t *testing.T){
-  if testing.Short(){
-    t.Skip("Skipping Test point subtract point in short suite")
-  }
-
+func Test_Point3D_Subtract_Point3D(t *testing.T){
   p1 := Point3D {X: 1, Y: 1, Z: 1}
   p2 := Point3D {X: 2, Y: 2, Z: 2}
   out:= Point3D {X: -1, Y: -1, Z: -1}
@@ -14,5 +10,34 @@ func TestSubtract(t *testing.T){
   if res:=p1.Subtract(p2); res != out{
     t.Errorf("(%v).Subtract(%v) => expected %v, got %v", p1, p2, out, res)
   }
+}
 
+func Test_Point3D_Add_Point3D(t *testing.T){
+  p1 := Point3D {X: 1, Y: 1, Z: 1}
+  p2 := Point3D {X: 2, Y: 2, Z: 2}
+  out:= Point3D {X: 3, Y: 3, Z: 3}
+
+  if res:=p1.Add(p2); res != out{
+    t.Errorf("(%v).Add(%v) => expected %v, got %v", p1, p2, out, res)
+  }
+}
+
+func Test_Point3D_Multiply_Float(t *testing.T){
+  p1 := Point3D {X: 1, Y: 1, Z: 1}
+  f := 2.5
+  out:= Point3D {X: 2.5, Y: 2.5, Z: 2.5}
+
+  if res:=p1.Multiply(f); res != out{
+    t.Errorf("(%v).Multiply(%v) => expected %v, got %v", p1, f, out, res)
+  }
+}
+
+func Test_Point3D_ScalarProd_Point3D(t *testing.T){
+  p1 := Point3D {X: 1, Y: 1, Z: 1}
+  p2 := Point3D {X: 5, Y: -3, Z: 4.5}
+  out:= 6.5
+
+  if res:=p1.ScalarProd(p2); res != out{
+    t.Errorf("(%v).ScalarProd(%v) => expected %v, got %v", p1, p2, out, res)
+  }
 }
